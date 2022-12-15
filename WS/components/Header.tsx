ccb80dfbc9a9d3c7eb0edd65820284a33730492a
imakero <45946863+imakero@users.noreply.github.com>
2022-12-15 16:35:41 +0100

    header always on top

--- WS/components/Header.tsx
+++ WS/components/Header.tsx
@@ -10,8 +10,8 @@ type HeaderProps = {
 
 const Header = ({ story }: HeaderProps) => {
   return (
-    <header className="flex justify-between items-center p-4 absolute w-full text-xl bg-primary">
-      <Link href="/" legacyBehavior>
+    <header className="flex justify-between items-center p-4 absolute w-full text-xl bg-primary z-10">
+      <Link href="http://www.github.com/imakero" target="_blank" legacyBehavior>
         <a className="flex items-center font-extralight text-white fill-white space-x-2 hover:text-secondary hover:fill-secondary">
           <GitHubIcon />
           <span>Imakero</span>
