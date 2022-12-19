a157fc1f274450670c1b9c2c28e05789000f58bb
imakero <45946863+imakero@users.noreply.github.com>
2022-12-19 13:08:00 +0100

    update form block component to take props

--- WS/block-components/Form.tsx
+++ WS/block-components/Form.tsx
@@ -1,38 +1,48 @@
 import { Blok, SbForm } from "../types/Types";
-import { Formik, Form as FormikForm } from "formik";
+import { Formik, Form as FormikForm, FormikHelpers } from "formik";
 import { StoryblokComponent, storyblokEditable } from "@storyblok/react";
 
 type FormProps = {
   blok: Blok<SbForm>;
+  initialValues: { [index: string]: string };
+  onSubmit: (
+    values: { [index: string]: string },
+    formikHelpers: FormikHelpers<{ [index: string]: string }>
+  ) => void | Promise<any>;
 };
 
-const Form = ({ blok }: FormProps) => {
-  const initialValues = blok.fields.reduce(
-    (values, nestedBlok) => ({ ...values, [nestedBlok.name]: "" }),
-    {}
-  );
-
+const Form = ({ blok, initialValues, onSubmit }: FormProps) => {
   return (
     <div className="p-4 w-80" {...storyblokEditable(blok)}>
-      <Formik
-        initialValues={initialValues}
-        onSubmit={(values) => console.log(values)}
-      >
-        <div className="space-y-6">
-          <FormikForm>
-            <div className="flex flex-col space-y-6 mb-6">
-              {blok.fields.map((nestedBlok) => (
-                <StoryblokComponent blok={nestedBlok} key={nestedBlok._uid} />
-              ))}
+      <Formik initialValues={initialValues} onSubmit={onSubmit}>
+        {({ status }) => {
+          return status?.status === "submitted" ? (
+            <div>{status.message}</div>
+          ) : (
+            <div className="space-y-6">
+              <FormikForm>
+                <div className="flex flex-col space-y-6 mb-6">
+                  {blok.fields.map((nestedBlok) => (
+                    <StoryblokComponent
+                      blok={nestedBlok}
+                      key={nestedBlok._uid}
+                    />
+                  ))}
+                </div>
+                <div className="text-red-600 mb-6">{status?.message}</div>
+                <button
+                  type="submit"
+                  className="bg-primary font-bold text-white rounded-full px-4 py-2 hover:scale-[102%] focus:outline-none focus:border-2 focus:border-primary-light"
+                  disabled={status?.status === "submitting"}
+                >
+                  {status?.status === "submitting"
+                    ? "sending..."
+                    : blok.submit_text}
+                </button>
+              </FormikForm>
             </div>
-            <button
-              type="submit"
-              className="bg-primary font-bold text-white rounded-full px-4 py-2 hover:scale-[102%] focus:outline-none focus:border-2 focus:border-primary-light"
-            >
-              {blok.submit_text}
-            </button>
-          </FormikForm>
-        </div>
+          );
+        }}
       </Formik>
     </div>
   );
